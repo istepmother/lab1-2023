350226091893c280b7b024d8fc2fad402dd37c22
istepmother <imachukha@kse.org.ua>
2023-03-05 21:38:06 +0100

    playing with tokenization (NOT) my code

--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
-﻿using System;
+﻿string Input()
+{
+    Console.Write("> ");
+    return Console.ReadLine()!;
+}
 
-Console.WriteLine("Hello, C#!");
+List<string> Tokenize(string r)
+{
+    string buffer = "";
+    List<string> result = new List<string>();
+
+    foreach (char s in r)
+    {
+        if (Char.IsNumber(s))
+        {
+            buffer += s;
+        }
+        else if (s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')')
+        {
+            if (buffer.Length > 0)
+            {
+                result.Add(buffer);
+                buffer = "";
+            }
+
+            result.Add(s.ToString());
+        }
+    }
+
+    if (buffer.Length > 0)
+    {
+        result.Add(buffer);
+    }
+
+    return result;
+}
+
+// Main
+
+string input = Input();
+List<string> tokens = Tokenize(input);
+
+Console.WriteLine(string.Join("|", tokens));
